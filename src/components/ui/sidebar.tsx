64f8e3d49f371cb668ca83e7dd670ee2cda925cb
Juan David Jiménez Aréchiga <davidjimenezar17@gmail.com>
2025-10-02 02:19:49 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -10,7 +10,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetClose } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -18,7 +18,6 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
-import { Dialog } from "@radix-ui/react-dialog"
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
@@ -207,7 +206,9 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
-            <Dialog.Title className="sr-only">Sidebar Navigation</Dialog.Title>
+            <SheetHeader className="p-2">
+              <SheetTitle>Sidebar Navigation</SheetTitle>
+            </SheetHeader>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
