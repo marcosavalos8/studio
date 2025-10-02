e540367aab8e1512a491a56949e9549953cfd186
Juan David Jiménez Aréchiga <davidjimenezar17@gmail.com>
2025-10-02 01:19:14 +0000

    Quiero que el lector del qr sea con la cámara trasera

--- src/app/(app)/time-tracking/page.tsx
+++ src/app/(app)/time-tracking/page.tsx
@@ -41,7 +41,7 @@ export default function TimeTrackingPage() {
   useEffect(() => {
     const getCameraPermission = async () => {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
         setHasCameraPermission(true);
 
         if (videoRef.current) {
