d797bca695d2c4e98f4dbb7ebe6f9e44da76effa
Juan David Jiménez Aréchiga <davidjimenezar17@gmail.com>
2025-10-03 21:08:44 +0000

    Nooooo its not, i understand you. Don't rush, please, I trust on you

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,5 @@ export const firebaseConfig = {
   "appId": "1:434983121945:web:86b1cd3bc62bff397fa688",
   "apiKey": "AIzaSyABQpfZcj8_Wj2zSEeWWAzl7FtCVE_ZL6o",
   "authDomain": "studio-3716014693-db09c.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "434983121945"
 };
