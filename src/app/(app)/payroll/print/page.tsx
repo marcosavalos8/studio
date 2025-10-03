c77c10687886bfcd7205373827590db7bd1f3f22
Juan David Jiménez Aréchiga <davidjimenezar17@gmail.com>
2025-10-03 21:59:48 +0000

    Try fixing this error: `Build Error: Error: x Expression expected. Error

--- src/app/(app)/payroll/print/page.tsx
+++ src/app/(app)/payroll/print/page.tsx
@@ -193,7 +193,7 @@ export default function PrintPayrollPage() {
 
     return (
         <>
-            <style jsx global>{\`
+            <style jsx global>{`
                 @media print {
                     body {
                         background-color: #fff;
@@ -206,7 +206,7 @@ export default function PrintPayrollPage() {
                         break-before: page;
                     }
                 }
-            \`}</style>
+            `}</style>
             <ReportToPrint report={report} />
         </>
     );
