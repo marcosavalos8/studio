4daba6d3a087874c9ed576db22f53f0a0c657e8b
marcosavalos8 <m.a.a.g.3008@gmail.com>
2025-10-20 13:32:17 -0600

    funcionando todo

--- next.config.ts
+++ next.config.ts
@@ -1,51 +1,13 @@
-import type {NextConfig} from 'next';
-import withPWA from '@ducanh2912/next-pwa';
-
-const pwa = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-    };
-    return config;
+  // Disable trace in development to avoid permission issues
+  experimental: {
+    trace: process.env.NODE_ENV === "production",
   },
 };
 
-export default pwa(nextConfig);
+module.exports = nextConfig;
